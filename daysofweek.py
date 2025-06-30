51886c5dbceb6994cd5229e6372f9f2fa8745cb0
installer <installer@gmail.com>
2025-06-30 13:01:09 +0530

    Changes to days of week and aplhabet

--- daysofweek.py
+++ daysofweek.py
@@ -1,4 +1,10 @@
+ch=int(input("Enter choice"))
 days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+if ch>=1 and ch<=7:
+    print(days[ch-1])
+else:
+    print("Error")
 
-for day in days:
-    print(day)
+
+
+
--- isalphabet.py
+++ isalphabet.py
@@ -1,9 +1,10 @@
 print("Enter a character")
 char=input()
-if char.isalpha():
-    if char.lower() in ['a', 'e', 'i', 'o', 'u']:
-        print(char, "is a vowel")
+vowels=[97,101,105,111,117,65,69,73,79,85]
+if ord(char)>=97 and ord(char)<=122 or ord(char)>=65 and ord(char)<=90:
+    if ord(char) in vowels:
+        print("Vowels")
     else:
-        print(char, "is a consonant")
+        print("Consonants")
 else:
-    print("Not an alphabet")
+    print("Error")
